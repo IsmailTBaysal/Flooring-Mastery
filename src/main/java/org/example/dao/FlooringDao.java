f5e0db65dbe8405044782435dfc8c6b9acdeff86
TheOptic57 <thecalebzhou@gmail.com>
2023-12-13 17:00:31 -0500

    Implemented FlooringDao.java
    Added all the methods in the interface

--- src/main/java/org/example/dao/FlooringDao.java
+++ src/main/java/org/example/dao/FlooringDao.java
@@ -1,4 +1,23 @@
 package org.example.dao;
 
+import org.example.dto.Order;
+import org.example.dto.Product;
+import org.example.dto.State;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface FlooringDao {
+    Order addOrder(Order order);
+    void editOrder(LocalDate date, int orderNum);
+    Order getOrder(LocalDate date, int orderNum);
+    Order removeOrder(LocalDate date, int orderNum);
+    List<Order> getAllOrder();
+    
+    State getState(String stateName);
+    State addState(String stateName, State state);
+
+    Product getProduct(String productName);
+    Product addProduct(String productName, Product product);
+    List<Product> getAllProduct();
 }
