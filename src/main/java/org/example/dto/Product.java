6cea202c49093c852cb8c20c4a3ce8a424e24609
IsmailTBaysal <i.timucinbaysal@gmail.com>
2023-12-14 09:48:31 -0600

    Product created

--- src/main/java/org/example/dto/Product.java
+++ src/main/java/org/example/dto/Product.java
@@ -2,5 +2,55 @@
 
 package org.example.dto;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private String ProductType;
+    private BigDecimal CostPerSquareFoot;
+    private BigDecimal LaborCostPerSquareFoot;
+    private BigDecimal MaterialCost;
+    private BigDecimal LaborCost;
+
+    public String getProductType() {
+        return ProductType;
+    }
+
+    public void setProductType(String productType) {
+        ProductType = productType;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return CostPerSquareFoot;
+    }
+
+    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
+        CostPerSquareFoot = costPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return LaborCostPerSquareFoot;
+    }
+
+    public void setLaborCostPerSquareFoot(BigDecimal laborCostPerSquareFoot) {
+        LaborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+
+    public BigDecimal getMaterialCost() {
+        return MaterialCost;
+    }
+
+    public void setMaterialCost(BigDecimal materialCost) {
+        MaterialCost = materialCost;
+    }
+
+    public BigDecimal getLaborCost() {
+        return LaborCost;
+    }
+
+    public void setLaborCost(BigDecimal laborCost) {
+        LaborCost = laborCost;
+    }
 }
+
+
+
